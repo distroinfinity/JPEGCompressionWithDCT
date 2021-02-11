4bb4407665e3a91e32aa6a4690dc001d0e3fcae6
distroinfinity <manurajput2911@gmail.com>
2021-02-12 03:27:59 +0530

    Added quantisation table for 10,50 and 90 percent compression

--- main.py
+++ main.py
@@ -2,30 +2,18 @@ import numpy
 from PIL import Image
 from scipy.fftpack import dct,idct
 
-#Load an image,change colour space from RGB to YCbCr
-#and store data in an array
+def quantisation(k):
+    #Quantisation table for Y component
+    Q10 = numpy.array([[80,60,50,80,120,200,255,255],
+                [55,60,70,95,130,255,255,255],
+                [70,65,80,120,200,255,255,255],
+                [70,85,110,145,255,255,255,255],
+                [90,110,185,255,255,255,255,255],
+                [120,175,255,255,255,255,255,255],
+                [245,255,255,255,255,255,255,255],
+                [255,255,255,255,255,255,255,255]])
 
-imgArray = numpy.array(Image.open("grcropped.jpg",'r').convert('YCbCr'))
-imgArray = imgArray.astype("float64")
-
-#YCbCr ranges from -127 to 128 unlike RGB which is 0-256
-Y=imgArray[:,:,0]-128
-#Cb=imgArray[:,:,1]-128
-#Cr=imgArray[:,:,2]-128
-
-blockSize=8
-imgSize = imgArray.shape
-height = imgSize[0]+(8-(imgSize[0]%8))
-width = imgSize[1]+(8-(imgSize[1]%8))
-dctOutput = numpy.zeros([height,width],float)
-for i in range(0,imgSize[0]):
-    for j in range(0,imgSize[1]):
-        dctOutput[i][j]=Y[i][j]
-#print(dctOutput)
-
-#Quantisation matrix for medium compression
-"""
-qt = numpy.array([[16,11,10,16,24,40,51,61],
+    Q50 = numpy.array([[16,11,10,16,24,40,51,61],
                 [12,12,14,19,26,58,60,55],
                 [14,13,16,24,40,57,69,56],
                 [14,17,22,29,51,87,80,62],
@@ -33,8 +21,8 @@ qt = numpy.array([[16,11,10,16,24,40,51,61],
                 [24,35,55,64,81,104,113,92],
                 [49,64,78,87,103,121,120,101],
                 [72,92,95,98,112,100,130,99]])
-"""
-qt = numpy.array([[3,2,2,3,5,8,10,12],
+
+    Q90 = numpy.array([[3,2,2,3,5,8,10,12],
                     [2,2,3,4,5,12,12,11],
                     [3,3,3,5,8,11,14,11],
                     [3,3,4,6,10,17,16,12],
@@ -42,41 +30,57 @@ qt = numpy.array([[3,2,2,3,5,8,10,12],
                     [5,7,11,13,16,12,23,18],
                     [10,13,16,17,21,24,24,21],
                     [14,18,19,20,22,20,20,20]])
-count=0
-print(height," ",width)
+    if k == "Q10":
+        return Q10
+    elif k == "Q50":
+        return Q50
+    elif k == "Q90":
+        return Q90
+    
 
-for i in range(0,height,8):
-    for j in range(0,width,8):
-        #performing out-place, for more clearity 
-        temp = dctOutput[i:i+8,j:j+8]
-        
-        #Apply dct on 8x8 matrix
-        temp = dct(dct(temp,axis=0,norm='ortho'),axis=1,norm='ortho')
-        #Quantize the DCT output
-        for k in range(0,8):
-            for m in range(0,8):
-                temp[k][m]=numpy.round(temp[k][m]/qt[k][m])
-        
-        dctOutput[i:i+8,j:j+8] = idct(idct(temp,axis=0,norm='ortho'),axis=1,norm='ortho')
+if __name__ == "__main__":
+    #load image & convert to YCbCr colourspcae
+    imgArray = numpy.array(Image.open("grcropped.jpg",'r').convert('YCbCr'))
+    imgArray = imgArray.astype("float64")
 
-        count+=1
-        print(count)
-        #print(temp)
+    #centre pixel values around 0
+    imgArray=imgArray[:,:,0]-128
+    #Cb=imgArray[:,:,1]-128
+    #Cr=imgArray[:,:,2]-128
 
-#print(dctOutput)
-dctOutput = dctOutput+128
-outputImage = Image.fromarray(dctOutput).convert("RGB")
-outputImage.save('output.jpg')
+    blockSize=8
+    imgSize = imgArray.shape
+    #make height and width a multiple of 8
+    height = imgSize[0]+(8-(imgSize[0]%8))
+    width = imgSize[1]+(8-(imgSize[1]%8))
+    
+    #outplace matrix to adjust the boundary cases
+    dctOutput = numpy.zeros([height,width],float)
+    for i in range(0,imgSize[0]):
+        for j in range(0,imgSize[1]):
+            dctOutput[i][j]=imgArray[i][j]
+    qt=quantisation("Q50")
+    
+    #Iterate on 8x8 pixel group
+    for i in range(0,height,8):
+        for j in range(0,width,8):
+            #performing out-place, for more clearity 
+            temp = dctOutput[i:i+8,j:j+8]
+            
+            #Apply dct on 8x8 matrix
+            temp = dct(dct(temp,axis=0,norm='ortho'),axis=1,norm='ortho')
+            #Quantize the DCT output
+            for k in range(0,8):
+                for m in range(0,8):
+                    temp[k][m]=numpy.round(temp[k][m]/qt[k][m])
+            
+            dctOutput[i:i+8,j:j+8] = idct(idct(temp,axis=0,norm='ortho'),axis=1,norm='ortho')
+
+    dctOutput = dctOutput+128
+    outputImage = Image.fromarray(dctOutput).convert("RGB")
+    outputImage.save('groutput.jpg')
 
 
-"""
-print(dctOutput[0:2,0:2])
-print(dctOutput[0:2,2:4])
-print(dctOutput[2:4,0:2])
-"""
 #Downsample the colour components by a factor of 2 in both direction
 #Cb=Cb[0::2,0::2]
 #Cr=Cr[0::2,0::2]
-
-#outputImage = Image.fromarray(imgArray)
-#outputImage.save('outputImage.jpeg')
